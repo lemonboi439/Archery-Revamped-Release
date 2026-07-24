39d34099e5d6135372873b7a6357c238f024433c
lemonboi439 <lemonboi439@gmail.com>
2026-07-24 12:21:06 +0100

    Base

---
+++ src/client/java/me/lemonboi439/archeryRevamped/client/ArcheryRevampedClient.java
@@ -0,0 +1,10 @@
+package me.lemonboi439.archeryRevamped.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArcheryRevampedClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
